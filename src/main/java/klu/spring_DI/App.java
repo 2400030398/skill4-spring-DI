5ac0678d524e11be78821b6901451f55c9673517
2400030398 <2400030398@kluniversity.in>
2026-01-21 16:00:43 +0530

    Update App.java

--- src/main/java/klu/spring_DI/App.java
+++ src/main/java/klu/spring_DI/App.java
@@ -6,17 +6,17 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Step 1: Load Spring IoC Container
+        
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        // Step 2: Get Employee Bean from Container
+        
         Employee emp = (Employee) context.getBean("emp1");
 
-        // Step 3: Call method to display values
+        
         emp.display();
 
-        // Step 4: Close the container (IMPORTANT)
+        
         context.close();
     }
 }
